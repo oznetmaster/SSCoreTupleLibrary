a5623efa4d7f963a62d00c9648571d82ba36bf25
oznetmaster <ncolvin@nivloc.com>
2019-05-21 10:38:25 +0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System;
+
+[assembly: AssemblyTitle ("SSCoreTupleLibrary")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSCoreTupleLibrary")]
+[assembly: AssemblyCopyright ("Copyright ©  2019")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+[assembly: CLSCompliant (true)]
+
